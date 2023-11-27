9c275f13fca0e0d3d84760999389cd15ef980fd2
RJ <rj@metabrew.com>
2023-11-27 14:52:09 +0000

    rm empty example

--- examples/server.rs
+++
@@ -1,9 +0,0 @@
-extern crate packeteer;
-
-fn main() {
-    // let mut app = App::new();
-    // app.add_plugins(DefaultPlugins);
-    // app.add_plugins(ReparateServerPlugin);
-    // app.add_systems(Update, print_packets);
-    // app.run();
-}
